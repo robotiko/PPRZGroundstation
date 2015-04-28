38b8a58d0f4364e7a4d09343b6b43cc1d146524a
svanlochem <svanlochem@gmail.com>
2015-04-28 12:06:43 +0200

    setText enabled

--- src/com/gcs/BatteryFragment.java
+++ src/com/gcs/BatteryFragment.java
@@ -6,16 +6,21 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 public class BatteryFragment extends Fragment {
+	View view;
+	
 	@Override
 	public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-		View view = inflater.inflate(R.layout.battery, container, false);
+		view = inflater.inflate(R.layout.battery, container, false);
 		return view;
 	}
 
 	public void setText(String item) {
-		// TODO: Dynamically set the text
+		
+		TextView textView = (TextView) view.findViewById(R.id.batteryValue);
+	    textView.setText(item);
 	}
 	
 	public void setBackground(Color color) {
