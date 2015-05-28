a15273f8bb7a36dbb258a9ae42155bb9b3644904
svanlochem <svanlochem@gmail.com>
2015-05-28 17:49:19 +0200

    Mission buttons improved

--- app/src/main/java/com/gcs/fragments/MissionButtonFragment.java
+++ app/src/main/java/com/gcs/fragments/MissionButtonFragment.java
@@ -9,36 +9,88 @@ import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
 public class MissionButtonFragment extends Fragment {
 	
 	private View rootView;
-	
+    private Button homeButton, landButton, takeOffButton;
+
+    private enum ButtonName {
+        HOME, LAND, TAKEOFF
+    }
+
 	@Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
         Bundle savedInstanceState) {
 		
 		// Inflate the layout for this fragment
 		rootView = inflater.inflate(R.layout.mission_buttons, container, false);
-        return rootView; 
+
+        return rootView;
 	}
+
+    @Override
+    public void onActivityCreated(Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+
+        // Create handles for the buttons
+        homeButton    = (Button) getView().findViewById(R.id.goHomeButton);
+        landButton    = (Button) getView().findViewById(R.id.landButton);
+        takeOffButton = (Button) getView().findViewById(R.id.takeOffButton);
+    }
 	
 	/* TODO implement actions for clicks on the mission buttons */
-	/* TODO make custom buttons to replace the current textbuttons, if clicked color changes */
+	/* TODO make custom buttons to replace the current text buttons, if clicked color changes */
 	
 	public void onLandRequest(View v) {
-        Log.d("COMMAND","LAND");
+
+        //Set button to active
+        setButtonAppearance(true, ButtonName.LAND);
     }
 	
 	public void onTakeOffRequest(View v) {
-		Log.d("COMMAND","TAKE-OFF");
+
+        //Set button to active
+        setButtonAppearance(true, ButtonName.TAKEOFF);
 	}
 
 	public void onGoHomeRequest(View v) {
-		Log.d("COMMAND","GO HOME");
+
+		//Set button to active
+        setButtonAppearance(true, ButtonName.HOME);
 	}
 
 	public void onWaypointRequest(View v) {
+
         Log.d("COMMAND", "Update waypoints");
+    }
+
+    // Method to change button appearance (active/inactive)
+    public void setButtonAppearance(Boolean active, ButtonName buttonName) {
+
+        switch (buttonName) {
+            case HOME:
+                if(active) {
+                    homeButton.setBackgroundResource(R.drawable.home_button_active);
+                } else {
+                    homeButton.setBackgroundResource(R.drawable.home_button_inactive);
+                }
+                break;
+            case LAND:
+                if(active) {
+                    landButton.setBackgroundResource(R.drawable.home_button_active);
+                } else {
+                    landButton.setBackgroundResource(R.drawable.home_button_inactive);
+                }
+                break;
+            case TAKEOFF:
+                if(active) {
+                    takeOffButton.setBackgroundResource(R.drawable.home_button_active);
+                } else {
+                    takeOffButton.setBackgroundResource(R.drawable.home_button_inactive);
+                }
+                break;
+        }
     }
 }
